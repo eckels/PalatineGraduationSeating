d390fe05d9fa5ef1b9fe2090f0e72e4ffee1f06e
Evan Eckels <evaneckels@Evans-MacBook-Pro-3.local>
2022-05-05 19:29:07 -0500

    Added seating chart capabilities

--- main.cpp
+++ main.cpp
@@ -10,17 +10,62 @@
 
 using namespace std;
 
-int NUM_ROWS = 0;
-int ROW_LENGTH = 0;
-//string FIRST_SIDE = "left";
+/**************************
+*
+* Configurations
+*
+**************************/
+
+// Attribute Keywords
+const string SENIOR_BOARD_KEYWORD = "Senior Board";
+const string SPEAKER_KEYWORD = "Speaker";
+const string ACADEMIC_SCHOLARS_KEYWORD = "Academic Scholar";
+const string SPED_REGULAR_KEYWORD = "SPED Regular";
+const string SPED_BACK_KEYWORD = "SPED Back";
+const string NOT_GRADUATING_KEYWORD = "Not Graduating";
+const string SIBLING_TWIN_KEYWORD = "Twins/Siblings";
+
+// Add-on Keywords
+const string SPED_HELPER_KEYWORD = "Helper";
+const string SPED_TA_KEYWORD = "TA";
+
+// File Names
+string STUDENT_IMPORT_NAME = "student_list.csv";
+const string LEFT_SIDE_SPEAKER_FILE_NAME = "left_side_speaker_list.txt";
+const string RIGHT_SIDE_SPEAKER_FILE_NAME = "right_side_speaker_list.txt";
+const string SEATING_CHART_FILE_NAME = "seating_chart.csv";
+
+// Print seat number alongside name or not (speaker list and seating chart)
+const bool SPEAKER_SEAT_NUM_FLAG = true;
+const bool CHART_SEAT_NUM_FLAG = true;
+
+// Number of faculty speakers on each side
+const int FACULTY_SPEAKER_SIDE_NUM = 2;
+
+// Stage rows and columns
+int NUM_ROWS = 38;
+int ROW_LENGTH = 9;
+
+// Banner space size for the Arena banner
+const int BANNER_SPACER_SIZE = 10;
+
+
+/**************************
+*
+* Data Structures
+*
+**************************/
 
 struct Student {
     string first;
     string last;
     string attribute;
     bool sibling;
+    bool helper;
+    int seat_number;
     Student() {
         sibling = false;
+        helper = false;
     }
 };
 
@@ -39,34 +84,25 @@ vector<Student*> STUDENTS_GRADUATING_ALPHABETICAL;
 vector<Student*> ORDERED_STUDENTS;
 
 // Secondary student orderings
-const string SENIOR_BOARD_KEYWORD = "Senior Board";
 vector<Student*> SENIOR_BOARD_LIST;
-
-const string SPEAKER_KEYWORD = "Speaker";
 vector<Student*> SPEAKER_LIST;
-
-const string ACADEMIC_SCHOLARS_KEYWORD = "Academic Scholar";
 vector<Student*> ACADEMIC_SCHOLARS_LIST;
-
-const string SPED_REGULAR_KEYWORD = "SPED Regular";
-const string SPED_HELPER_KEYWORD = "SPED Back";
 vector<Student*> SPED_LIST;
-
-const string NOT_GRADUATING_KEYWORD = "Excluded";
 vector<Student*> EVERYONE_ELSE_LIST;
-
-const string SIBLING_TWIN_KEYWORD = "Sibling";
 
 // Inside of row is always front of vector
 vector<vector<Seat*> > LEFT_SIDE;
 vector<vector<Seat*> > RIGHT_SIDE;
 
-// Misc
-//string NEXT_SIDE = FIRST_SIDE;
 queue<Student*> SIBLING_QUEUE;
 // Negative if more on left, positive if more on right. If zero, put on left
 int SIDE_DIFF = 0;
-int BANNER_SPACER_SIZE = 10;
+
+/**************************
+*
+* Main Functions
+*
+**************************/
 
 // Create empty candidate objects
 void generate_side(vector<vector<Seat*> >& side, bool first) {
@@ -83,36 +119,10 @@ void generate_side(vector<vector<Seat*> >& side, bool first) {
     }
 }
 
-bool compareStudents(const Student& a, const Student& b) {
-    string aname = a.last;
-    string bname = b.last;
-    transform(aname.begin(), aname.end(), aname.begin(), ::tolower);
-    transform(bname.begin(), bname.end(), bname.begin(), ::tolower);
-
-    int lastname = aname.compare(bname);
-    if (lastname < 0) {
-        return true;
-    } else if (lastname > 0) {
-        return false;
-    } else {
-        aname = a.first;
-        bname = b.first;
-        transform(aname.begin(), aname.end(), aname.begin(), ::tolower);
-        transform(bname.begin(), bname.end(), bname.begin(), ::tolower);
-
-        int firstname = aname.compare(bname);
-        if (firstname > 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-}
-
 void generate_banner_list() {
     for (int i = 0; i < RAW_STUDENTS.size(); i++) {
         Student* current = RAW_STUDENTS.at(i);
-        if (current->attribute.find(NOT_GRADUATING_KEYWORD) == string::npos) {
+        if (current->attribute.find(NOT_GRADUATING_KEYWORD) == string::npos && current->helper != true) {
             STUDENTS_GRADUATING_ALPHABETICAL.push_back(current);
         }
     }
@@ -145,7 +155,7 @@ void sort_students() {
             ACADEMIC_SCHOLARS_LIST.push_back(current);
         } else if (current->attribute.find(SPED_REGULAR_KEYWORD) != string::npos) {
             EVERYONE_ELSE_LIST.push_back(current);
-        } else if (current->attribute.find(SPED_HELPER_KEYWORD) != string::npos) {
+        } else if (current->attribute.find(SPED_BACK_KEYWORD) != string::npos) {
             SPED_LIST.push_back(current);
         } else if (current->attribute.find(NOT_GRADUATING_KEYWORD) == string::npos) {
             EVERYONE_ELSE_LIST.push_back(current);
@@ -156,6 +166,7 @@ void sort_students() {
 void assign_seat(Seat* seat, Student* student) {
     seat->student = student;
     seat->occupied = true;
+    student->seat_number = seat->seat_number;
 }
 
 bool bound_checker(vector<vector<Seat*> >& to_assign, int& row, int& column, string direction) {
@@ -174,7 +185,6 @@ bool bound_checker(vector<vector<Seat*> >& to_assign, int& row, int& column, str
 }
 
 bool seating_helper(int& lrow, int& rrow, int& lcolumn, int& rcolumn, vector<Student*>& students, string direction) {
-    //bool side = (NEXT_SIDE == "left") ? true : false;
     bool side = true;
     if (SIDE_DIFF >= 0) {
         side = true;
@@ -209,7 +219,6 @@ bool seating_helper(int& lrow, int& rrow, int& lcolumn, int& rcolumn, vector<Stu
                 } else {
                     SIDE_DIFF += 2;
                 }
-                //NEXT_SIDE = (NEXT_SIDE == "left") ? "right" : "left";
                 return true;
             }
         }
@@ -218,7 +227,6 @@ bool seating_helper(int& lrow, int& rrow, int& lcolumn, int& rcolumn, vector<Stu
         students.erase(students.begin());
         column += 1;
 
-        //NEXT_SIDE = (NEXT_SIDE == "left") ? "right" : "left";
         if (side) {
             SIDE_DIFF -= 1;
         } else {
@@ -254,12 +262,136 @@ void seat_from_front(vector<Student*>& students) {
 
 void populate_seating_chart() {
     seat_from_back(SPED_LIST);
-    //NEXT_SIDE = FIRST_SIDE;
     seat_from_front(SENIOR_BOARD_LIST);
     seat_from_front(SPEAKER_LIST);
     seat_from_front(ACADEMIC_SCHOLARS_LIST);
     seat_from_front(EVERYONE_ELSE_LIST);
 }
+
+void import_students() {
+    vector<vector<string> > content;
+	vector<string> row;
+    string line;
+    string word;
+
+    fstream file (STUDENT_IMPORT_NAME, ios::in);
+
+    if(file.is_open()) {
+        while (getline(file, line)) {
+            row.clear();
+            stringstream str(line);
+            while(getline(str, word, ',')) {
+                row.push_back(word);
+            }
+            content.push_back(row);
+        }
+    } else {
+        cout << "Could not open the file\n";
+    }
+    for (int i = 0; i < content.size(); i++) {
+        Student* student = new Student();
+        student->last = content[i][0];
+        student->first = content[i][1];
+        if (content[i].size() > 2) {
+            student->attribute = content[i][2];
+            if (content[i][2].find(SIBLING_TWIN_KEYWORD) != string::npos) {
+                student->sibling = true;
+            }
+            if (content[i][2].find(SPED_HELPER_KEYWORD) != string::npos) {
+                student->sibling = true;
+            }
+            if (content[i][2].find(SPED_TA_KEYWORD) != string::npos) {
+                student->helper = true;
+            }
+        }
+        RAW_STUDENTS.push_back(student);
+    }
+}
+
+vector<vector<Student*> > generate_speaker_lists(vector<vector<Seat*> >& seating, int rows, int speakers) {
+    vector<vector<Student*> > lists;
+    vector<Student*> current;
+
+    // This rounds it up
+    int chunk_size = (rows + speakers - 1) / speakers;
+
+    for (int i = 0; i < NUM_ROWS; i++) {
+        for (int j = 0; j < ROW_LENGTH; j++) {
+            if (seating[i][j]->occupied) {
+                if (seating[i][j]->student->helper != true) {
+                    current.push_back(seating[i][j]->student);
+                }
+            }
+        }
+        if ((i + 1) % chunk_size == 0) {
+            lists.push_back(current);
+            current.clear();
+        }
+    }
+    if (current.size() > 0) {
+        lists.push_back(current);
+    }
+    return lists;
+}
+
+void print_speaker_list(vector<vector<Student*> >& lists, string filename, bool seat_nums) {
+
+    ofstream outfile (filename);
+
+    for (int i = 0; i < lists.size(); i++) {
+        outfile << "------ NEW SPEAKER ------" << endl;
+        for (int j = 0; j < lists[i].size(); j++) {
+            Student* student = lists[i][j];
+            if (seat_nums) {
+                outfile << student->seat_number << " - ";
+            }
+            outfile << student->first << " " << student->last << endl;
+        }
+    }
+
+    outfile.close();
+}
+
+void print_seating_chart(string filename, bool seat_nums) {
+    ofstream outfile (filename);
+
+    for (int i = 0; i < NUM_ROWS; i++) {
+        for (int j = ROW_LENGTH - 1; j >= 0; j--) {
+            if (seat_nums) {
+                outfile << LEFT_SIDE[i][j]->seat_number << " ";
+            }
+            if (LEFT_SIDE[i][j]->occupied) {
+                outfile << LEFT_SIDE[i][j]->student->first << " " << LEFT_SIDE[i][j]->student->last << ",";
+            } else {
+                outfile << "Empty" << ",";
+            }
+        }
+
+        outfile << "-,-,";
+
+        for (int j = 0; j < ROW_LENGTH; j++) {
+            if (seat_nums) {
+                outfile << RIGHT_SIDE[i][j]->seat_number << " ";
+            }
+            if (RIGHT_SIDE[i][j]->occupied) {
+                outfile << RIGHT_SIDE[i][j]->student->first << " " << RIGHT_SIDE[i][j]->student->last << ",";
+            } else {
+                outfile << "Empty" << ",";
+            }
+        }
+
+        outfile << "\n";
+    }
+
+    outfile.close();
+}
+
+
+/**************************
+*
+* Debug Print Functions
+*
+**************************/
 
 string five_printer(string print) {
     string res = print;
@@ -271,7 +403,7 @@ string five_printer(string print) {
     return res;
 }
 
-void print_chart() {
+void debug_chart() {
     for (int i = 0; i < NUM_ROWS; i++) {
         for (int j = 0; j < ROW_LENGTH; j++) {
             cout << " ----- ";
@@ -337,97 +469,53 @@ void print_chart() {
     }
 }
 
-void print_student_list(vector<Student*>& list) {
+void debug_student_list(vector<Student*>& list) {
     for (int i = 0; i < list.size(); i++) {
         cout << list[i]->last << ", " << list[i]->first << " - " << list[i]->attribute << endl;
     }
 }
 
-void import_students() {
-    vector<vector<string> > content;
-	vector<string> row;
-    string line;
-    string word;
-
-    fstream file ("shortdata.csv", ios::in);
-
-    if(file.is_open()) {
-        while (getline(file, line)) {
-            row.clear();
-            stringstream str(line);
-            while(getline(str, word, ',')) {
-                row.push_back(word);
-            }
-            content.push_back(row);
-        }
-    } else {
-        cout << "Could not open the file\n";
-    }
-    for (int i = 0; i < content.size(); i++) {
-        Student* student = new Student();
-        student->last = content[i][0];
-        student->first = content[i][1];
-        if (content[i].size() > 2) {
-            student->attribute = content[i][2];
-            if (content[i][2].find(SIBLING_TWIN_KEYWORD) != string::npos) {
-                student->sibling = true;
-            }
-        }
-        RAW_STUDENTS.push_back(student);
-    }
-}
-
-vector<vector<Student*> > generate_speaker_lists(vector<vector<Seat*> >& seating, int rows) {
-    vector<vector<Student*> > lists;
-    vector<Student*> current;
-    for (int i = 0; i < NUM_ROWS; i++) {
-        for (int j = 0; j < ROW_LENGTH; j++) {
-            if (seating[i][j]->occupied) {
-                current.push_back(seating[i][j]->student);
-            }
-        }
-        if ((i + 1) % rows == 0) {
-            lists.push_back(current);
-            current.clear();
-        }
-    }
-    if (current.size() > 0) {
-        lists.push_back(current);
-    }
-    return lists;
-}
-
-void read_speaker_lists(vector<vector<Student*> >& lists) {
+void debug_speaker_lists(vector<vector<Student*> >& lists) {
     for (int i = 0; i < lists.size(); i++) {
         cout << "------ NEW SPEAKER ------" << endl;
-        print_student_list(lists[i]);
+        debug_student_list(lists[i]);
     }
 }
+
+
+/**************************
+*
+* Program Operations
+*
+**************************/
 
 int main(int argc, char *argv[]) {
     // Read students from CSV
     import_students();
-    //print_student_list(RAW_STUDENTS);
 
     // Create the banner
     generate_banner_list();
     print_banner_file();
 
     // Set up seating
-    NUM_ROWS = 5;
-    ROW_LENGTH = 5;
     generate_side(LEFT_SIDE, true);
     generate_side(RIGHT_SIDE, false);
 
+    // Put students in seats
     sort_students();
-    //print_student_list(SENIOR_BOARD_LIST);
-    print_student_list(RAW_STUDENTS);
     populate_seating_chart();
-    print_chart();
 
-    vector<vector<Student*> > left_lists = generate_speaker_lists(LEFT_SIDE, 4);
-    vector<vector<Student*> > right_lists = generate_speaker_lists(RIGHT_SIDE, 4);
-    read_speaker_lists(left_lists);
-    read_speaker_lists(right_lists);
+    // Printing debug stuff
+    //debug_student_list(RAW_STUDENTS);
+    //debug_chart();
+
+    // Print speaker lists
+    vector<vector<Student*> > left_lists = generate_speaker_lists(LEFT_SIDE, NUM_ROWS, FACULTY_SPEAKER_SIDE_NUM);
+    vector<vector<Student*> > right_lists = generate_speaker_lists(RIGHT_SIDE, NUM_ROWS, FACULTY_SPEAKER_SIDE_NUM);
+    print_speaker_list(left_lists, LEFT_SIDE_SPEAKER_FILE_NAME, SPEAKER_SEAT_NUM_FLAG);
+    print_speaker_list(right_lists, RIGHT_SIDE_SPEAKER_FILE_NAME, SPEAKER_SEAT_NUM_FLAG);
+
+    // Generate CSV of seating chart
+    print_seating_chart(SEATING_CHART_FILE_NAME, CHART_SEAT_NUM_FLAG);
     return 0;
 }
